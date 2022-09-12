a7081a6375fdd3fda825f6d9e3619cd97dfa1011
Sachita Nand <shachitanandk@gmail.com>
2022-09-13 00:00:00 +0530

    Band Name Generator.py

---
+++ Band Name Generator.py
@@ -0,0 +1,4 @@
+print("Welcome to Band Name Generator :")
+city=input ("Which city you grew up:\n")
+pet_name=input("What is the name of your pet:\n")
+print('Your band name could be '+city+" "+pet_name)
