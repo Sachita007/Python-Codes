00f58ccb99a22a1c408f9c9b398bf99130b631a2
Sachita Nand <shachitanandk@gmail.com>
2022-09-15 00:00:00 +0530

    days_and _Weeks Left.py

---
+++ days_and _Weeks Left.py
@@ -0,0 +1,6 @@
+age=int(input("Enter your age :"))
+years_left=90-age
+months_left=years_left*12
+days_left=years_left*365
+weeks_left=years_left*52
+print(f"You have {days_left} days, {weeks_left} weeks and {months_left} months left.")
